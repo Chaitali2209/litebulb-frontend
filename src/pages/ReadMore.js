d0e6f8e48c71c738c906b108c99183aaeb96e2dc
Chaitali2209 <chaitalipadalkar.it@bvcoenm.edu.in>
2023-04-06 23:25:22 +0530

    final commit

--- src/pages/ReadMore.js
+++ src/pages/ReadMore.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Readmore.css'
 
 const ReadMore = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className='about'>
+
+      <img src=''></img>
+      <button className='build'> <span className='text'>Build</span> </button>
+      <button className='vouch'> <span>vouch</span> </button>
+      <h2 className='head-title'>A plarform for token gated scheduled booking.</h2>
+
+
     </div>
   )
 }
